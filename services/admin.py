0736d0ca84d1359d828e2a05cb27421210c94efa
Dmitriy8854 <dmit.morozoff2009@yandex.ru>
2023-09-16 23:15:28 +0300

    Settings templates and static

--- services/admin.py
+++ services/admin.py
@@ -3,15 +3,15 @@ from django.contrib import admin
 # Register your models here.
 
 
-from services.models import Subscription
+# from services.models import Subscription
 
 
-class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ("name", "price")
-    list_filter = ("name", "price")
+# class SubscriptionAdmin(admin.ModelAdmin):
+#     list_display = ("name", "price")
+#     list_filter = ("name", "price")
 
-    # def count_favorites(self, obj):
-    #     return obj.favorites.count()
+#     # def count_favorites(self, obj):
+#     #     return obj.favorites.count()
 
 
-admin.site.register(Subscription, SubscriptionAdmin)
+# admin.site.register(Subscription, SubscriptionAdmin)
--- src/settings.py
+++ src/settings.py
@@ -17,7 +17,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -62,7 +62,7 @@ ROOT_URLCONF = "src.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
